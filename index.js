12a58ee88c16242fd7c69bbdf2e6621abd933c63
Ashraful Islam <155339396+theashrafislam@users.noreply.github.com>
2024-06-13 10:56:56 +0600

    overall sever site make done

--- index.js
+++ index.js
@@ -94,6 +94,7 @@ async function run() {
         app.post('/users', async (req, res) => {
             const user = req.body;
             // insert email if user doesnt exists:
+            // insert email if user doesnt exists:
             // you can do this many ways(1. email unique, 2. upsert 3. simple checking)
             const query = { email: user.email };
             const existsEmail = await usersCollection.findOne(query);
